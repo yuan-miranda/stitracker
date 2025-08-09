97dbb27941e8b5838d921aa650657a933d2267ed
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-08-09 14:37:19 +0800

    also removed this

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import {
 
 // export const iframeHeight = "800px"
 
-export const description = "A sidebar with a header and a search form."
+// export const description = "A sidebar with a header and a search form."
 
 export default function Page() {
   return (
